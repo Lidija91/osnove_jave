321db277c4cc03737f968303636d69f48e51e4e7
Lidija Milivojevic <l.milivojevic29@gmail.com>
2022-05-17 19:47:59 +0200

    Zadatak 1 od 16.05.2022.

---
+++ osnove_jave/src/main/java/d_16_05_2022/Zadatak1.java
@@ -0,0 +1,15 @@
+package d_16_05_2022;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+//        Napisati metodu stampajVrednostZa10Vecu u kojoj se stampa prosledjena vrednost za 10 veca.
+//        Iz main-a pozvati izvrsenje metode za razlicite vrednosti.
+
+        System.out.println(stampajVrednostZa10Vecu(5));
+    }
+
+    static int stampajVrednostZa10Vecu(int a) {
+        return (a + 10);
+
+    }
+}
