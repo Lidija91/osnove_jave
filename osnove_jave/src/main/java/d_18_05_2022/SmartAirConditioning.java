d69f04001d55d6b1fad043ce1bf1185e6cfa9486
Lidija Milivojevic <l.milivojevic29@gmail.com>
2022-05-23 17:56:07 +0200

    Zadatak za vezbanje

--- osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioning.java
+++ osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioning.java
@@ -4,13 +4,31 @@ public class SmartAirConditioning {
     public String marka;
     public int temperatura;
     public String mod;
+    public int potrosnjaDokHladi;
+    public int potrosnjaDokGreje;
 
+    public SmartAirConditioning () {
 
-    public void stampa () {
-        if (temperatura>=16 && temperatura<=35 && mod.equals("hladi") || mod.equals("greje"))
-            System.out.println(marka);
-        System.out.println(temperatura);
-        System.out.println(mod);
+    }
+    public SmartAirConditioning(String marka, int temperatura, String mod, int potrosnjaDokHladi, int potrosnjaDokGreje) {
+        this.marka = marka;
+        this.temperatura = temperatura;
+        this.mod = mod;
+        this.potrosnjaDokHladi = potrosnjaDokHladi;
+        this.potrosnjaDokGreje = potrosnjaDokGreje;
     }
 
+    public void stampa() {
+        System.out.println(marka);
+        System.out.println(mod);
+        System.out.println(temperatura);
+    }
+
+    public int mesecnaPotrosnja ()
+    {int potrosnja;
+        if (mod.equals("greje")) {
+        potrosnja = 30*15*potrosnjaDokGreje;}
+        else potrosnja = 30*15*potrosnjaDokHladi;
+    return potrosnja;
+    }
 }
--- osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioningMain.java
+++ osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioningMain.java
@@ -7,13 +7,18 @@ public class SmartAirConditioningMain {
         prvaKlima.marka = "Samsung";
         prvaKlima.temperatura = 20;
         prvaKlima.mod = "hladi";
+        prvaKlima.potrosnjaDokHladi = 100;
+        prvaKlima.potrosnjaDokGreje = 200;
 
         prvaKlima.stampa();
+        System.out.println();
 
         SmartAirConditioning drugaKlima = new SmartAirConditioning();
         drugaKlima.marka = "Galanz";
         drugaKlima.temperatura = 30;
         drugaKlima.mod = "greje";
         drugaKlima.stampa();
+        int potrosnja = prvaKlima.mesecnaPotrosnja();
+        System.out.println("Potrosnja je " + potrosnja);
     }
 }
