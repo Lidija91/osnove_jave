0fca99d5a8e6225a53baa22a53a0f12ee6e3e50f
Lidija Milivojevic <l.milivojevic29@gmail.com>
2022-05-25 13:01:22 +0200

    domaci 24.05.2022.

---
+++ osnove_jave/src/main/java/d_24_05_2022/ZeleniKarton.java
@@ -0,0 +1,68 @@
+package d_24_05_2022;
+
+public class ZeleniKarton {
+    private String imeIPrezime;
+    private String brojIndeksa;
+    private String nazivPredmeta;
+    private String imeProfesora;
+    private int ocena;
+
+    public ZeleniKarton(String imeIPrezime, String brojIndeksa, String nazivPredmeta, String imeProfesora, int ocena) {
+        this.imeIPrezime = imeIPrezime;
+        this.brojIndeksa = brojIndeksa;
+        this.nazivPredmeta = nazivPredmeta;
+        this.imeProfesora = imeProfesora;
+        this.ocena = ocena;
+    }
+
+    public String getImeIPrezime() {
+        return imeIPrezime;
+    }
+
+    public void setImeIPrezime(String imeIPrezime) {
+        this.imeIPrezime = imeIPrezime;
+    }
+
+    public String getBrojIndeksa() {
+        return brojIndeksa;
+    }
+
+    public void setBrojIndeksa(String brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
+
+    public String getNazivPredmeta() {
+        return nazivPredmeta;
+    }
+
+    public void setNazivPredmeta(String nazivPredmeta) {
+        this.nazivPredmeta = nazivPredmeta;
+    }
+
+    public String getImeProfesora() {
+        return imeProfesora;
+    }
+
+    public void setImeProfesora(String imeProfesora) {
+        this.imeProfesora = imeProfesora;
+    }
+
+    public int getOcena() {
+        return ocena;
+    }
+
+    public void setOcena(int ocena) {
+        this.ocena = ocena;
+    }
+    public boolean polozenIspit () {
+        return ocena > 5;
+    }
+    public void stampaj () {
+        System.out.println(nazivPredmeta + " " + ocena);
+        System.out.println("Student: " + imeIPrezime + ", " + brojIndeksa);
+        System.out.println("Profesor: " + imeProfesora);
+    }
+
+
+
+}
