3028c6f7a8264ef2dd6f5d5b9d541f482fe1e3d4
Lidija Milivojevic <l.milivojevic29@gmail.com>
2022-05-30 23:20:06 +0200

    zadatak Instagram 30.05.2022.

---
+++ osnove_jave/src/main/java/p_30_05_2022/InstagramMain.java
@@ -0,0 +1,15 @@
+package p_30_05_2022;
+
+public class InstagramMain {
+    public static void main(String[] args) {
+
+        InstagramUser Jovana = new InstagramUser("Jo", "Jovana Delic", "jdelic@gmail.com");
+        InstagramUser Saska = new InstagramUser("Sashka", "Aleksandra Nesovic", "sashka@hotmail.com");
+        InstagramUser Cira = new InstagramUser("Cira", "Ognjen Ciric", "ogi@gmail.com");
+
+        InstagramTag x = new InstagramTag(5, 6);
+        InstagramImage slika1 = new InstagramImage("1000mpx", "instagram.com/korisnik-Saska/profilna.jpg");
+
+
+    }
+}
